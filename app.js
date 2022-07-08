2c97130221ba3aa7cf5b95853d585cb91e7ea0bf
jorgelinadev <jorgelinalberti@gmail.com>
2022-07-08 09:09:54 -0300

    First commit

---
+++ app.js
@@ -0,0 +1,11 @@
+//alert ("Mi archivo está bien linkeado")
+
+const dividir = (a,b) => {
+    const valor1 = a ;
+    const valor2 = b ; 
+    return a / b ;
+}
+
+console.log (dividir (10,2));
+console.log (dividir (12,2));
+console.log (dividir (18,2));
